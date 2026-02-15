e3004e1c17b2a9b7503fca504be81660d388c48c
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 19:34:23 +0300

    Implement printThreeWords method to print fruits

--- Task1.java
+++ Task1.java
@@ -1,1 +1,12 @@
-# QAQ-Aston
+// Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+
+public class FruitPrinter {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void main(String args) {
+        printThreeWords();
+    }
+}
