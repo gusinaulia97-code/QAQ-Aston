ea94ba9cc117c63c3e838323ac5043986ed74d01
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 20:05:05 +0300

    Double values less than 6 in the array
    Iterate through the array and double values less than 6.

--- Task12.java
+++ Task12.java
@@ -1,1 +1,15 @@
+// Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2.
 
+public class Massive12 {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for(int i = 0; i < array.length; i++){
+            if (array[i] < 6){
+                array[i] *= 2;
+            }
+        }
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+    }
+}
