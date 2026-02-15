ff215c231137912e46604cd64800b3a48cbd363e
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 21:05:46 +0300

    Add method to create an int array with initial values
    Implemented a method to create an int array with a specified length and initial value.

--- Task14.java
+++ Task14.java
@@ -1,1 +1,17 @@
+// Написать метод, принимающий на вход два аргумента: len и initialValue, 
+// и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue.
 
+public static int[] retLenArr (int len, int initialValue){
+    int arr[] = new int[len];
+    for (int i = 0; i < arr.length; i++){
+        arr[i] = initialValue;
+    }
+    return arr;
+}
+public static void printArrayInConsole(int[] inputArray) {
+    for (int i = 0; i < inputArray.length; i++) {
+        System.out.print(inputArray[i] + " ");
+    }
+    System.out.println();
+}
+printArrayInConsole(retLenArr(длинна массива, значение индексов));
