3ec580ae6073b1a7b43f099f6bc8faff97ef60ed
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 20:00:21 +0300

    Add isNegative method to check number sign
    Implement method to check if a number is negative.

--- Task7.java
+++ Task7.java
@@ -1,1 +1,13 @@
+// Напишите метод, которому в качестве параметра передается целое число. Метод должен вернуть true, 
+// если число отрицательное, и вернуть false если положительное. Замечание: ноль считаем положительным числом.
 
+public class NumberChecker {
+    public static boolean isNegative(int number) {
+        return number < 0;
+    }
+    public static void main(String args) {
+        System.out.println(isNegative(-25));  // true
+        System.out.println(isNegative(0));   // false
+        System.out.println(isNegative(16));  // false
+    }
+}
