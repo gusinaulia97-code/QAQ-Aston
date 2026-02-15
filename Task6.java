1024703ba54138f11dc4d410aad9acf20f129de9
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 19:59:20 +0300

    Add method to check if a number is positive or negative

--- Task6.java
+++ Task6.java
@@ -1,1 +1,17 @@
+// Напишите метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, 
+// положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
 
+public class NumberChecker {
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("Передано положительное число или ноль: " + number);
+        } else {
+            System.out.println("Передано отрицательное число: " + number);
+        }
+    }
+    public static void main(String args) {
+        checkNumber(8);    // Положительное число
+        checkNumber(-10);   // Отрицательное число
+        checkNumber(0);    // Ноль положительное число
+    }
+}
