997538b06371952a49bd39fcf29174b790b71f5a
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 19:57:07 +0300

    Update Task4.java

--- Task4.java
+++ Task4.java
@@ -1,1 +1,18 @@
+// Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b, 
+// и инициализируйте их любыми значениями, которыми захотите. Если a больше или равно b, 
+// то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”.
 
+public class NumberComparison {
+    public static void compareNumbers() {
+        int a = 48;
+        int b = 1;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+    public static void main(String args) {
+        compareNumbers();
+    }
+}
