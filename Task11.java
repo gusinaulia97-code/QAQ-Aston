b7801d1df5f4dfae60baea60f17363cf86ed9e28
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 20:04:17 +0300

    Fill integer array with values from 1 to 100
    Initialized an integer array and filled it with values from 1 to 100 using a loop.

--- Task11.java
+++ Task11.java
@@ -1,1 +1,13 @@
+// Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100
 
+public class Massive11 {
+    
+    public static void main(String[] args) {
+        
+        int[] array = new int[101];
+
+        for(int i = 1; i < array.length; i++){
+            System.out.print((array[i] = i) + " ");
+        }
+    }
+}
