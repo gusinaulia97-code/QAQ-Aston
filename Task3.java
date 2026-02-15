b577a84257cb7b4bfcaa7895ba637c8df309f05f
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 19:54:16 +0300

    Implement printColor method for color output

--- Task3.java
+++ Task3.java
@@ -1,1 +1,20 @@
+// Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением. 
+// Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”, 
+// если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”, 
+// если больше 100 (100 исключительно) – “Зеленый”.
 
+public class ColorPrinter {
+    public static void printColor() {
+        int value = 33;
+        if (value < 0) {
+            System.out.println("Красный");
+        } else if (value >= 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void main(String args) {
+        printColor();
+    }
+}
