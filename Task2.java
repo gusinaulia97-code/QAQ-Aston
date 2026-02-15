63906c13cfc52b5ea154e9701930bb7b4ae014c2
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 19:52:44 +0300

    Implement checkSumSign method to evaluate sum sign

--- Task2.java
+++ Task2.java
@@ -1,1 +1,20 @@
+//Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, 
+// и инициализируйте их любыми значениями, которыми захотите. Далее метод должен просуммировать эти переменные,
+// и если их сумма больше или равна 0, то вывести в консоль сообщение “Сумма положительная”,
+// в противном случае - “Сумма отрицательная”.
 
+ public class SumChecker {
+    public static void checkSumSign() {
+        int a = 14;
+        int b = -8;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void main(String args) {
+        file = checkSumSign();
+    }
+}
