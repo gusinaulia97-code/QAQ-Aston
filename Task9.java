87710128cfe9188e150eaf1ee3521ef3d160a66a
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-15 20:02:27 +0300

    Add leap year checker method in LeapYearChecker
    Implement method to check for leap years.

--- Task9.java
+++ Task9.java
@@ -1,1 +1,14 @@
+// Напишите метод, который определяет, является ли год високосным, и возвращает boolean
+// (високосный - true, не високосный - false). Каждый 4-й год является високосным, кроме каждого 100-го, 
+// при этом каждый 400-й – високосный.
 
+public class LeapYearChecker {
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+    public static void main(String args) {
+        System.out.println(isLeapYear(2400));  // true (делится на 400)
+        System.out.println(isLeapYear(1500));  // false (делится на 100, но не на 400)
+        System.out.println(isLeapYear(2000));  // true (делится на 4, не делится на 100)
+    }
+}
