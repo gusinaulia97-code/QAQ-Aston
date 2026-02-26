bbc427718b83506ea3fdfb43b3bd7912d3b656df
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-26 12:04:41 +0300

    Update Task6.java

--- Task6.java
+++ Task6.java
@@ -1,7 +1,7 @@
 // Напишите метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, 
 // положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
 
-public class NumberChecker {
+public class Task6.java {
     public static void checkNumber(int number) {
         if (number > 0) {
             System.out.println("Передано положительное число или ноль: " + number);
