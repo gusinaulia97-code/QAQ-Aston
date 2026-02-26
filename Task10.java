028f2f902dcad7fd2411a214b5a6189109e20358
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-26 11:58:39 +0300

    Rename class to Task10.java and update comments
    Renamed class from 'Main' to 'Task10.java' and updated comments.

--- Task10.java
+++ Task10.java
@@ -1,7 +1,7 @@
 // Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. 
 // С помощью цикла и условия заменить 0 на 1, 1 на 0.
 
-public class Main {
+public class Task10.java {
     public static void main(String[] args) {
 	int[] arr1 = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
     for (int i = 0; i < arr1.length; i++) {
