be16b1cd001a79cc03efd1c334c7a56301d8d8df
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-26 12:29:47 +0300

    Update Task10.java

--- Task10.java
+++ Task10.java
@@ -2,11 +2,17 @@
 // С помощью цикла и условия заменить 0 на 1, 1 на 0.
 
 public class Task10.java {
-    public static void main(String[] args) {
-	int[] arr1 = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-    for (int i = 0; i < arr1.length; i++) {
-        if (arr1[i] == 1) {
+    public static void main(String args) {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < array.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+        }
+        for(int value : arr) {
+            System.out.print(value + " ");
         }
     }
-}
 }
