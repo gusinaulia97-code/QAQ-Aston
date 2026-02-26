12256d54f6ee8ac96c34eae73332abe06f4a5bd7
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-26 12:04:16 +0300

    Update Task5.java

--- Task5.java
+++ Task5.java
@@ -1,7 +1,7 @@
 // Напишите метод, принимающий на вход два целых числа и проверяющий, 
 // что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
 
-public class SumChecker {
+public class Task5.java {
     public static boolean checkSum(int a, int b) {
         int sum = a + b;
         return sum >= 10 && sum <= 20;
