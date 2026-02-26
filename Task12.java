1f39b64829486ce8329857ac1b3f8177b89ae5ef
gusinaulia97-code <gusinaulia97@gmail.com>
2026-02-26 12:00:15 +0300

    Fix class name in Task12.java
    Corrected class name from 'Massive12' to 'Task12.java'.

--- Task12.java
+++ Task12.java
@@ -1,6 +1,6 @@
 // Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2.
 
-public class Massive12 {
+public class Task12.java {
     public static void main(String[] args) {
         int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for(int i = 0; i < array.length; i++){
